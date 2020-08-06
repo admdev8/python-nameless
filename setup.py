56fe4bf7410e834865fd820fbe28fab15c0a162f
Your Name <you@example.com>
2020-08-06 11:01:50 +0000

    Bump version: 1.0.0 → 1.694.4

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def read(*names, **kwargs):
 
 setup(
     name='nameless',
-    version='1.0.0',
+    version='1.694.4',
     license='BSD-2-Clause',
     description='An example package. Generated with cookiecutter-pylibrary.',
     long_description='%s\n%s' % (
